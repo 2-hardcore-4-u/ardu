78319683d74524613cae0b7cc8b6b353f9fe3cd2
Nikita Smolyaninov <nikita.smolaninov@gmail.com>
2021-05-13 22:52:31 +0300

    первый и, возможно, последний коммит тута

---
+++ ardu/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+namespace Program
+{
+    class Program
+    {
+        static SerialPort _serialPort;
+        public static void Main()
+        {
+            _serialPort = new SerialPort();
+            _serialPort.BaudRate = 9600;
+            Console.WriteLine("Вкажіть номер порту підключення: ");
+            _serialPort.PortName = "COM" + Console.ReadLine().ToString();
+            _serialPort.Open();
+            while (true)
+            {
+                string a = _serialPort.ReadExisting();
+                Console.WriteLine(a);
+                Thread.Sleep(120);
+            }
+        }
+    }
+}
